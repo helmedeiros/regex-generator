3cfbf9889d86ef462f5c854d01a170428aa23f66
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2018-01-22 18:13:46 -0300

    feat: add application shell

---
+++ src/app/App.test.tsx
@@ -0,0 +1,13 @@
+import { shallow } from "enzyme";
+import * as React from "react";
+import { App } from "./App";
+
+describe("App", () => {
+  it("renders the title", () => {
+    expect(shallow(<App />).find(".app-title").text()).toBe("Regex Generator");
+  });
+
+  it("renders a header", () => {
+    expect(shallow(<App />).find(".app-header").length).toBe(1);
+  });
+});
---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1 className="app-title">Regex Generator</h1>
+      </header>
+    </div>
+  );
+}
