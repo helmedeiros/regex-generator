f94530b1940535b745bd9586e515b70d397a68b4
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2018-01-23 21:10:41 -0300

    feat: hold sample text in application state

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,6 +1,8 @@
 import { shallow } from "enzyme";
 import * as React from "react";
+import { SampleInput } from "../components/SampleInput";
 import { App } from "./App";
+import { defaultSample } from "./defaultSample";
 
 describe("App", () => {
   it("renders the title", () => {
@@ -11,7 +13,17 @@ describe("App", () => {
     ).toBe("Regex Generator");
   });
 
-  it("renders a header", () => {
-    expect(shallow(<App />).find(".app-header").length).toBe(1);
+  it("starts with the default sample text", () => {
+    expect(
+      shallow(<App />)
+        .find(SampleInput)
+        .prop("value")
+    ).toBe(defaultSample);
+  });
+
+  it("updates the sample when the input changes", () => {
+    const wrapper = shallow(<App />);
+    wrapper.find(SampleInput).simulate("change", "new text");
+    expect(wrapper.find(SampleInput).prop("value")).toBe("new text");
   });
 });
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,31 @@
 import * as React from "react";
+import { SampleInput } from "../components/SampleInput";
+import { Step } from "../components/Step";
+import { defaultSample } from "./defaultSample";
 
-export function App() {
-  return (
-    <div className="app">
-      <header className="app-header">
-        <h1 className="app-title">Regex Generator</h1>
-      </header>
-    </div>
-  );
+export interface AppState {
+  sample: string;
+}
+
+export class App extends React.Component<{}, AppState> {
+  public state: AppState = { sample: defaultSample };
+
+  public render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h1 className="app-title">Regex Generator</h1>
+        </header>
+        <main className="app-steps">
+          <Step index={1} title="Paste a sample text.">
+            <SampleInput value={this.state.sample} onChange={this.handleChange} />
+          </Step>
+        </main>
+      </div>
+    );
+  }
+
+  private handleChange = (sample: string) => {
+    this.setState({ sample });
+  };
 }
