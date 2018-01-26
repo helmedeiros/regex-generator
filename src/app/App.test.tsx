5e91e6b49474910d2a19d8f3d401f82626412fbf
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2018-01-26 20:43:21 -0300

    feat: show suggestions for the sample text

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,6 +1,8 @@
 import { shallow } from "enzyme";
 import * as React from "react";
 import { SampleInput } from "../components/SampleInput";
+import { Suggestions } from "../components/Suggestions";
+import { SuggestionBox } from "../suggestions/layout";
 import { App } from "./App";
 import { defaultSample } from "./defaultSample";
 
@@ -29,5 +31,12 @@ describe("App", () => {
 
   it("shows a hint for the sample step", () => {
     expect(shallow(<App />).find(".sample-hint").length).toBe(1);
+  });
+
+  it("shows suggestions for the sample", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Suggestions).length).toBe(1);
+    const boxes = wrapper.find(Suggestions).prop("boxes") as SuggestionBox[];
+    expect(boxes.length).toBeGreaterThan(0);
   });
 });
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,22 @@
 import * as React from "react";
 import { SampleInput } from "../components/SampleInput";
 import { Step } from "../components/Step";
+import { Suggestions } from "../components/Suggestions";
+import { recognize } from "../recognizers/recognize";
+import { Match } from "../regex/match";
+import { layoutSuggestions, SuggestionBox } from "../suggestions/layout";
 import { defaultSample } from "./defaultSample";
 
 export interface AppState {
   sample: string;
+  selected: Match[];
 }
 
 export class App extends React.Component<{}, AppState> {
-  public state: AppState = { sample: defaultSample };
+  public state: AppState = { sample: defaultSample, selected: [] };
 
   public render() {
+    const boxes = layoutSuggestions(recognize(this.state.sample));
     return (
       <div className="app">
         <header className="app-header">
@@ -23,12 +29,26 @@ export class App extends React.Component<{}, AppState> {
               Give us an example of the text you want to match using your regex.
             </p>
           </Step>
+          <Step index={2} title="Which parts of the text are interesting for you?">
+            <Suggestions text={this.state.sample} boxes={boxes} onSelect={this.handleSelect} />
+          </Step>
         </main>
       </div>
     );
   }
 
   private handleChange = (sample: string) => {
-    this.setState({ sample });
+    this.setState({ sample, selected: [] });
+  };
+
+  private handleSelect = (box: SuggestionBox) => {
+    const match = box.match;
+    const exists = this.state.selected.some(
+      picked =>
+        picked.start === match.start && picked.end === match.end && picked.name === match.name
+    );
+    if (!exists) {
+      this.setState({ selected: this.state.selected.concat(match) });
+    }
   };
 }
