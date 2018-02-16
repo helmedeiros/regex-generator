37cb02b14630846ea3a04f3361edf61e2da4450a
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2018-02-16 20:43:38 -0300

    feat: add tour popover

---
+++ src/components/Tour.test.tsx
@@ -0,0 +1,38 @@
+import { mount } from "enzyme";
+import * as React from "react";
+import { tourSteps } from "../app/tour";
+import { Tour } from "./Tour";
+
+describe("Tour", () => {
+  it("starts on the first step", () => {
+    expect(
+      mount(<Tour steps={tourSteps} onClose={jest.fn()} />)
+        .find(".tour-title")
+        .text()
+    ).toBe("Sample");
+  });
+
+  it("advances with next", () => {
+    const wrapper = mount(<Tour steps={tourSteps} onClose={jest.fn()} />);
+    wrapper.find(".tour-next").simulate("click");
+    expect(wrapper.find(".tour-title").text()).toBe("Recognition");
+  });
+
+  it("closes from the close button", () => {
+    const onClose = jest.fn();
+    mount(<Tour steps={tourSteps} onClose={onClose} />)
+      .find(".tour-close")
+      .simulate("click");
+    expect(onClose).toBeCalled();
+  });
+
+  it("finishes from the last step", () => {
+    const onClose = jest.fn();
+    const wrapper = mount(<Tour steps={tourSteps} onClose={onClose} />);
+    wrapper.find(".tour-next").simulate("click");
+    wrapper.find(".tour-next").simulate("click");
+    wrapper.find(".tour-next").simulate("click");
+    wrapper.find(".tour-done").simulate("click");
+    expect(onClose).toBeCalled();
+  });
+});
---
+++ src/components/Tour.tsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+import { TourStep } from "../app/tour";
+
+export interface TourProps {
+  steps: TourStep[];
+  onClose: () => void;
+}
+
+export interface TourState {
+  index: number;
+}
+
+export class Tour extends React.Component<TourProps, TourState> {
+  public state: TourState = { index: 0 };
+
+  public render() {
+    const { steps } = this.props;
+    const step = steps[this.state.index];
+    const isFirst = this.state.index === 0;
+    const isLast = this.state.index === steps.length - 1;
+    return (
+      <div className="tour">
+        <div className="tour-card">
+          <h3 className="tour-title">{step.title}</h3>
+          <p className="tour-body">{step.body}</p>
+          <div className="tour-actions">
+            <button type="button" className="tour-close" onClick={this.props.onClose}>
+              Close
+            </button>
+            <button
+              type="button"
+              className="tour-previous"
+              disabled={isFirst}
+              onClick={this.previous}
+            >
+              ← Previous
+            </button>
+            {isLast ? (
+              <button type="button" className="tour-done" onClick={this.props.onClose}>
+                Done
+              </button>
+            ) : (
+              <button type="button" className="tour-next" onClick={this.next}>
+                Next →
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  private previous = () => {
+    this.setState({ index: Math.max(0, this.state.index - 1) });
+  };
+
+  private next = () => {
+    this.setState({ index: Math.min(this.props.steps.length - 1, this.state.index + 1) });
+  };
+}
